2fe0243ba93deb1cd2f93483fac6fd9c3854b20c
riyanachwani <riyanachwani22@gmail.com>
2024-08-27 20:18:10 +0530

    Theme Page Created

---
+++ lib/utils/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static String landingPage = '/landing';
+  static String signupPage = '/signup';
+  static String loginPage = '/login';
+  static String forgotpasswordPage = '/forgotpassword';
+  static String dashboardPage = '/dashboard';
+}
---
+++ lib/widgets/themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Themes {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF2596BE)),
+        fontFamily: GoogleFonts.arOneSans().fontFamily,
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          elevation: 0.0,
+          foregroundColor: Colors.white,
+        ),
+      );
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        fontFamily: GoogleFonts.arOneSans().fontFamily,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          elevation: 0.0,
+          foregroundColor: Colors.black,
+        ),
+      );
+}
